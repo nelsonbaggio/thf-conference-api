950476c470cecbaf68ee33b0693b89a30a22af46
nelson.baggio <nelson.baggio@gmail.com>
2018-06-05 00:50:54 -0300

    create files for session and speaker domain

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {createConnection, Connection} from "typeorm";
+
+
 
 async function bootstrap() {
+  const connection: Connection = await createConnection({
+      type: "mongodb",
+      host: "localhost",
+      port: 27017,
+      database: "test"
+  });
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
